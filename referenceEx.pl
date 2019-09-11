64283739480639cd1b17cd991265e7ed811deb0c
choolake <choolake@gmail.com>
2019-09-11 10:16:43 +0530

    hash ferenceing added

--- referenceEx.pl
+++ referenceEx.pl
@@ -26,3 +26,24 @@ for (@$ar) {
   print "$ar->[$i++]\n"
 
 }
+
+
+my %month=(
+	'Jan' => 1,
+	'Feb' => 2,
+	'Mar' => 3,
+	'Apr' => 4,
+	'May' => 5,
+	'Jun' => 6,
+	'Jul' => 7,
+	'Aug' => 8,
+	'Sep' => 9,
+	'Oct' => 10,
+	'Nov' => 11,
+	'Dec' => 12,
+);
+my $monthr=\%month;
+for (keys %$monthr){
+  print "$_=$monthr->{$_}\n"
+
+}

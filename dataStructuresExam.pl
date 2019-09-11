c4bbcedcd821d1499ecd9902c429283ba9950759
choolake <choolake@gmail.com>
2019-09-11 14:32:12 +0530

    Linked list added

--- dataStructuresExam.pl
+++ dataStructuresExam.pl
@@ -22,3 +22,18 @@ print "after Push Stack:@queue\n";
 shift(@queue);
 print "after Pop Stack:@queue\n";
 print "end of Stack FIFO\n";
+
+print "--------------Making a Linked list with ref\n";
+my @links= qw(2 Null 1);
+print "Using Pointers\n";
+my @nodes= qw(Finger:Null whois:Null who:Null);
+for (my $i=0; $i<=$#nodes;$i++){
+  my $ptr=\$nodes[$i];
+  my @data=split(/:/,$$ptr);# split inedxed data by :
+  print "Before: $ptr @data";
+  $data[1]=$links[$i]; #add the link data to 1st entry of data 
+  print "->@data\n";
+  $$ptr=join ':',@data;
+}
+print "@nodes";
+print "end of Linkd List\n";
